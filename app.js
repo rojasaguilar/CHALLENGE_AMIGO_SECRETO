e9b5bbeef7bd03e435fd9a7d555f6a079370ec95
Ramses Rojas <rojas.aguilar.ramses721@gmail.com>
2025-07-25 15:44:50 -0700

    almost finished

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+const lista = document.querySelector(".name-list");
+
+function agregarAmigo() {
+  const input = document.getElementById("amigo");
+
+  const amigoTemp = input.value.trim();
+  if (amigoTemp === "") {
+    alert("Ingresa un nombre válido");
+    return;
+  }
+  if (amigos.find((amigo) => amigo === amigoTemp)) {
+    input.value = "";
+    alert("Amigo ya ingresado");
+    return;
+  }
+
+  amigos.push(amigoTemp);
+  lista.innerHTML += `<li>${amigoTemp}</li>`;
+  input.value = "";
+  return;
+}
+
+function sortearAmigo() {
+  if (isEmpty()) {
+    alert("Debes agregar amigos antes")
+    return;
+  }
+
+  const labelResultado = document.getElementById("resultado");
+  const amigoElegido = parseInt(Math.random() * amigos.length);
+  lista.innerHTML = "";
+  labelResultado.innerHTML = `El amigo secreto es: ${amigos[amigoElegido]}`;
+  return;
+}
+
+function isEmpty() {
+    return amigos.length === 0;
+}
